d4036ca894c189061a03dd8fd4abaefd6e30e0b0
Jose Pablo Granados <jpgcodecr@gmail.com>
2016-09-30 15:58:33 -0600

    Adding first files structure

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# higher-order-functions
+#Higher order functions
+
+<p align="center">
+    <img height="271" width="405" src="https://raw.githubusercontent.com/jpgcode/respondTo/master/demo/intro.jpg">
+</p>
+
+> Experiments with higher order functions (array methods)
+
+##Map
+It iterates over an array and return a new array with the changes you do to it.
+
+We have this array:
+```
+var animals = [
+	{name: 'Milo', type: 'cat', eyes: 2},
+	{name: 'Peluza', type: 'cat', eyes: 2},
+	{name: 'Yummy', type: 'cat', eyes: 2},
+	{name: 'Doggy', type: 'dog', eyes: 2},
+	{name: 'Sammy', type: 'dog', eyes: 2}
+];
+```
+
+Then we use the map method:
+```
+var getAnimals = animals.map(animal => `${animal.name} is a ${animal.type}`);
+```
+
+This returns the following:
+```
+[ 'Milo is a cat',
+  'Peluza is a cat',
+  'Yummy is a cat',
+  'Doggy is a dog',
+  'Sammy is a dog' ]
+```
+
+## Authors
+
+**Jose Pablo Granados**
+ 
++ [github/jpgcode](https://github.com/jpgcode)
++ [twitter/jpgcode](http://twitter.com/jpgcode) 
+
+## License
+
+Copyright (c) 2016 Jose Pablo Granados
+Released under the MIT license
---
+++ map.js
@@ -0,0 +1,87 @@
+'use strict';
+
+//Using array methods
+
+var higherOrderFunctionsModule = (function(){
+
+	var animals = [
+		{name: 'Milo', type: 'cat', eyes: 2},
+		{name: 'Peluza', type: 'cat', eyes: 2},
+		{name: 'Yummy', type: 'cat', eyes: 2},
+		{name: 'Doggy', type: 'dog', eyes: 2},
+		{name: 'Sammy', type: 'dog', eyes: 2}
+	];
+
+	var init = function(){
+		//Get all animals (map)
+		console.log('---');
+		console.log('all animals (map)');
+		console.log(getAnimals);
+
+		//Get all animals (forEach)
+		console.log('---');
+		console.log('all animals (forEach)'); 
+		console.log(getAnimals2);
+
+		//Get only dogs
+		console.log('---');
+		console.log('only dogs');
+		console.log(dogs);
+
+		//Get only sammy
+		console.log('---');
+		console.log('only one item'); 
+		console.log(sammy);
+
+		//Get all the names together
+		console.log('---');
+		console.log('fusionAnimals');
+		console.log(fusionedAnimals);
+
+		//Check if some of the animals are cats
+		console.log('---');
+		console.log('check if there are cats');
+		console.log(thereAreCats);
+
+		//Check if all the items are dogs
+		console.log('---');
+		console.log('check if ALL are dogs');
+		console.log(areAllDogs);
+	}
+
+	//Map example
+	var getAnimals = animals.map(animal => `${animal.name} is a ${animal.type}`);
+
+	//Foreach example
+	var getAnimals2 = animals.forEach((animal) => `${animal.name} is a ${animal.type}`);
+
+	/*
+	The "side effect" with forEach is that the original array is being changed. 
+	"No side effect" with map means that, in idiomatic usage, the original array elements are not changed.
+	*/
+
+	//Filter example
+	var dogs = animals.filter(animal => animal.type === 'dog');
+
+	//Find example
+	var sammy = animals.find(animal => animal.name === 'Sammy');
+
+	//Some example
+	var thereAreCats = animals.some(animal => animal.type === 'cat');
+
+	//Every example
+	var areAllDogs = animals.every(animal => animal.eyes === 2);
+
+	//Reduce example
+	var fusionedAnimals = animals.reduce((newName, animal) => newName+animal.name, '');
+
+
+	//Auto execute itself
+	init();
+
+})();
+
+
+//
+//http://eloquentjavascript.net/05_higher_order.html
+
